7bcac715d5591e961bd804543d181c05af61d14e
sourab <sourab@tuna.group>
2025-08-09 20:32:26 +0530

    email fix

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('email')->unique()->nullable();
+            $table->string('email')->nullable();
             $table->string('mobile_number')->nullable();
             $table->string('user_unique_id')->nullable();
             $table->string('wallet_address');
